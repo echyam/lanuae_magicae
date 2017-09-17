af217ffab33436b0c6b75a08e06239ff209d1908
MichaelBingleBongle <mjnovet@gmail.com>
2017-09-17 10:39:05 -0400

    wall sliding coming along

--- Teleporting Platformer/Assets/Scripts/PlayerController.cs
+++ Teleporting Platformer/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,14 @@ public class PlayerController : MonoBehaviour {
 
 	public float walkSpeed;
 	public float jumpHeight;
+	public float slideSpeed;
+	public float wallJumpHeight;
+	public float wallJumpAngle;
 
 	private Rigidbody2D myBody;
 	private bool isGrounded;
+	private bool isWalledL;
+	private bool isWalledR;
 
 	// Use this for initialization
 	void Start () {
@@ -18,17 +23,39 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		isGrounded = false;
+		isWalledL = false;
+		isWalledR = false;
 		RaycastHit2D groundedCheck = Physics2D.Raycast (transform.position,transform.up*-1.0f,0.53f);
-		if(groundedCheck.collider!=null/*&&groundedCheck.collider.tag!="Player"*/){
+		if(groundedCheck.collider!=null){
 			isGrounded=true;
 		}
-		Debug.Log ("Grounded" + isGrounded);
+		RaycastHit2D walledLCheck = Physics2D.Raycast (transform.position,transform.right*-1.0f,0.53f);
+		if(walledLCheck.collider!=null){
+			isWalledL=true;
+		}
+		RaycastHit2D walledRCheck = Physics2D.Raycast (transform.position,transform.right,0.53f);
+		if(walledRCheck.collider!=null){
+			isWalledR=true;
+		}
+
+		Debug.Log ("Walled Left:" + isWalledL + "Walled Right: " + isWalledR);
 
 		Vector2 resultVelo = myBody.velocity;
 		resultVelo.x = Input.GetAxis ("Horizontal") * walkSpeed;
 		if(Input.GetKeyDown("space")&&isGrounded){
 			resultVelo.y = jumpHeight;
 		}
+
+		if(isWalledL){
+			if(Input.GetAxis("Horizontal")==-1){
+				resultVelo.y = slideSpeed;
+			}
+			if(Input.GetKeyDown("space")){
+				resultVelo.x = wallJumpHeight * Mathf.Cos (wallJumpAngle);
+				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpHeight);
+			}
+		}
+
 		myBody.velocity = resultVelo;
 	}
 }
