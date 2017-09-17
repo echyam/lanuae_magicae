91b2ca9c9b6ebcd41167096d7954298c6bb6cacf
MichaelBingleBongle <mjnovet@gmail.com>
2017-09-17 15:06:38 -0400

    Player controller done. code should be good and we'd just need to adjust public variables in order make it play just right. Only thing that I might change is adding a clamp to horizontal velocity when you jump off the ground

--- Teleporting Platformer/Assets/Scripts/PlayerController.cs
+++ Teleporting Platformer/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour {
 	public float walkSpeed;
 	public float jumpHeight;
 	public float airAccel;
+	public float airSpeedLimit;
 	public float slideSpeed;
 	public float wallJumpHeight;
 	public float wallJumpAngle;
@@ -43,7 +44,7 @@ public class PlayerController : MonoBehaviour {
 		//[AIR MOVEMENT] Accelerate horizontal velocity as though in the air.  This change will be overwritten later if touching ground
 		Vector2 resultVelo = myBody.velocity;
 		resultVelo.x += Input.GetAxis ("Horizontal") * airAccel * Time.deltaTime;
-		resultVelo.x = Mathf.Clamp (resultVelo.x,-walkSpeed,walkSpeed);
+		resultVelo.x = Mathf.Clamp (resultVelo.x,-airSpeedLimit,airSpeedLimit);
 
 		//[GROUND MOVEMENT] If touching ground, instantly jump to full speed movement in desired direction as though running. When space is pressed, instantly change vertical velocity as though jumping
 		if(isGrounded){
@@ -53,47 +54,52 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 
-		//[WALL MOVEMENT] If touching a wall,
+		//[WALL MOVEMENT] If touching a wall, the act of pressing the horizontal button in the direction of the wall should limit downard velocity to no more than slideSpeed as though clining to the wall. 
+		//When space is pressed, set horizontal and vertical velocity such that speed is wallJumpSpeed and direction is wallJumpAngle above horizontal line as though kicking of the wall to jump.
 		if(isWalledL){
 			if(Input.GetAxis("Horizontal")==-1){
+				resultVelo.y += -9.81f * Time.deltaTime;
 				resultVelo.y = Mathf.Max (resultVelo.y,slideSpeed);
+
 			}
-			if(Input.GetKeyDown("space")){
-				resultVelo.x = wallJumpHeight * Mathf.Cos (wallJumpAngle);
-				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpHeight);
+			if(Input.GetKeyDown("space")&&!isGrounded){
+				resultVelo.x = wallJumpHeight * Mathf.Cos (wallJumpAngle*Mathf.PI);
+				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpAngle*Mathf.PI);
 			}
 		}
 
 		if(isWalledR){
 			if(Input.GetAxis("Horizontal")==1){
+				resultVelo.y += -9.81f * Time.deltaTime;
 				resultVelo.y = Mathf.Max (resultVelo.y,slideSpeed);
 			}
-			if(Input.GetKeyDown("space")){
-				resultVelo.x = wallJumpHeight * -1.0f * Mathf.Cos (wallJumpAngle);
-				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpHeight);
+			if(Input.GetKeyDown("space")&&!isGrounded){
+				resultVelo.x = wallJumpHeight * -1.0f * Mathf.Cos (wallJumpAngle*Mathf.PI);
+				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpAngle*Mathf.PI);
 			}
 		}
 
 		myBody.velocity = resultVelo;
 	}
 
-/*	IEnumerator wallJumpL(){
-		Vector2 resultVelo = myBody.velocity;
-			while(isWalledL){
-				resultVelo.x = wallJumpHeight * Mathf.Cos (wallJumpAngle);
-				resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpHeight);
-			yield return null;
-			}
-		myBody.velocity = resultVelo;
-	}*/
-
-/*	IEnumerator wallJumpR(){
-		Vector2 resultVelo = myBody.velocity;
-		while(isWalledR){
-			resultVelo.x = wallJumpHeight * -1.0f * Mathf.Cos (wallJumpAngle);
-			resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpHeight);
-			yield return null;
+	//Coroutines I thought I needed at a time put now it seems I don't I'll likely delete them later but for now holding on just to be sure
+/*	private IEnumerator wallJumpL(){
+		while(isWalledL){
+			Vector2 resultVelo = myBody.velocity;
+			resultVelo.x = wallJumpHeight * Mathf.Cos (wallJumpAngle*Mathf.PI);
+			resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpAngle*Mathf.PI);
+			myBody.velocity = resultVelo;
+			yield return 0;
 		}
-		myBody.velocity = resultVelo;
+	}
+
+	private IEnumerator wallJumpR(){
+		while(isWalledR){
+			Vector2 resultVelo = myBody.velocity;
+			resultVelo.x = wallJumpHeight * -1.0f * Mathf.Cos (wallJumpAngle*Mathf.PI);
+			resultVelo.y = wallJumpHeight * Mathf.Sin (wallJumpAngle*Mathf.PI);
+			myBody.velocity = resultVelo;
+			yield return 0;
+		}
 	}*/
 }
