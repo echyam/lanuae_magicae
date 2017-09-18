b9b143e63e9b0a49917a6c825956da93c9f7d5f9
MichaelBingleBongle <mjnovet@gmail.com>
2017-09-18 05:32:02 -0400

    started work on kunai stuff

---
+++ Teleporting Platformer/Assets/Scripts/KunaiManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KunaiManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 direcVec = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+		float deg = (Mathf.Atan2 (direcVec.y, direcVec.x) * Mathf.Rad2Deg)/* - 90.0f*/;
+		transform.rotation = Quaternion.AngleAxis (deg, transform.forward);
+	}
+}
