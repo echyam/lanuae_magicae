a23744e2c4b95cefbdd6e4f39d7b5e9388783f62
echyam <echyam@gmail.com>
2017-09-20 00:38:31 +0200

    add spinner

---
+++ Teleporting Platformer/Assets/Scripts/SpinnerScript.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerScript : MonoBehaviour {
+	private const float radius = 1f;
+	private const float wallLen = 2.5f;
+	private bool prevHit = false;
+
+	public GameObject[] walls;
+	private GameObject[] blades;
+
+	// Use this for initialization
+	void Start () {
+		float angle = 360f / walls.Length;
+		blades = new GameObject[walls.Length];
+		Quaternion rot;
+		Vector2 initial = new Vector2 (radius + wallLen/2f, 0);
+		Vector2 displacement;
+		GameObject blade;
+		for(int i = 0; i < walls.Length; i++) {
+			rot = Quaternion.AngleAxis(i*angle,transform.forward);
+			displacement = rot * initial;
+			blade = Instantiate (walls [i], (Vector2)transform.position + displacement, rot);
+			blades [i] = blade;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		bool hit = false;
+		foreach (GameObject blade in blades) {
+			hit = hit || blade.GetComponent<Walls> ().isHit ();
+		}
+		if (hit && !prevHit) {
+			rotateBlades ();
+		}
+		prevHit = hit;
+	}
+	private void OnTriggerEnter2D(Collider2D other){
+	}
+	private void rotateBlades() {
+		Vector2 tempPos = blades [0].transform.position;
+		Quaternion tempRot = blades [0].transform.rotation;
+		int i = 0;
+		while (i < blades.Length - 1) {
+			blades [i].transform.position = blades [i + 1].transform.position;
+			blades [i].transform.rotation = blades [i + 1].transform.rotation;
+			i++;
+		}
+		blades [i].transform.position = tempPos;
+		blades [i].transform.rotation = tempRot;
+	}
+}
--- Teleporting Platformer/Assets/Scripts/SwitcherooScript.cs
+++ Teleporting Platformer/Assets/Scripts/SwitcherooScript.cs
@@ -8,22 +8,21 @@ public class SwitcherooScript : MonoBehaviour {
 	private Collider2D switcherooCol;
 	private SpriteRenderer switcherooSR;
 	private int wallTypeInd = 0;
-	private bool prevCollision1 = false;
-	private bool prevCollision2 = false;
+	private bool prevCollision1 = false;	// so kunai can pass through without changing behavior
+	private bool prevCollision2 = false;	// so kunai can pass through without changing behavior
 	private Color revColor = new Color (144f/255f, 35f/255f, 35f/255f, 255f/255f);
 	private Color lightColor = new Color (186f/255f, 183f/255f, 79f/255f, 255f/255f);
 	private Color fastColor = new Color (88f/255f, 146f/255f, 86f/255f, 255f/255f);
 	private Color slowColor = new Color (68f/255f, 149f/255f, 204f/255f, 255/255f);
 
 	public GameObject[] wallTypes;
-	public string label;
 
 	// Use this for initialization
 	void Start () {
 		switcherooRB = GetComponent<Rigidbody2D> ();
 		switcherooCol = GetComponent<Collider2D> ();
 		switcherooSR = GetComponent<SpriteRenderer> ();
-		this.tag = label;
+		this.tag = wallTypes[0].tag;
 	}
 
 	// Update is called once per frame
@@ -45,6 +44,7 @@ public class SwitcherooScript : MonoBehaviour {
 			switcherooSR.color = slowColor;
 			break;
 		}
+
 		if (prevCollision1 && prevCollision2) {
 			prevCollision2 = false;
 		}
