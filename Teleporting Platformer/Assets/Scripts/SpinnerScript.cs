f2d09e284a918fb676fee32dd2338b0f7ffa182d
echyam <echyam@gmail.com>
2017-09-21 18:46:46 +0200

    fix spinner to move

--- Teleporting Platformer/Assets/Scripts/SpinnerScript.cs
+++ Teleporting Platformer/Assets/Scripts/SpinnerScript.cs
@@ -16,13 +16,14 @@ public class SpinnerScript : MonoBehaviour {
 		float angle = 360f / walls.Length;
 		blades = new GameObject[walls.Length];
 		Quaternion rot;
-		Vector2 initial = new Vector2 (radius + wallLen/2f, 0);
+		Vector2 initial = new Vector2 (0, 0);
 		Vector2 displacement;
 		GameObject blade;
 		for(int i = 0; i < walls.Length; i++) {
 			rot = Quaternion.AngleAxis(i*angle,transform.forward);
 			displacement = rot * initial;
 			blade = Instantiate (walls [i], (Vector2)transform.position + displacement, rot);
+			blade.GetComponent<Rigidbody2D> ().angularVelocity = 30;
 			blades [i] = blade;
 		}
 	}
