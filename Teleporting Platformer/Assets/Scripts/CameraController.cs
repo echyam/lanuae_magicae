2f38ce17c2d76dce456857e93079cdc673e03e9f
echyam <echyam@gmail.com>
2017-09-21 20:02:00 +0200

    add camera following and put sample levels in folder

---
+++ Teleporting Platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿// created with help from Unity tutorial
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public float minX;
+	public float maxX;
+	public float minY;
+	public float maxY;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 playerPos = player.transform.position;
+		float x = Mathf.Max(playerPos.x,minX);
+		x = Mathf.Min (x, maxX);
+		float y = Mathf.Max(playerPos.y, minY);
+		y = Mathf.Min (y, maxY);
+		transform.position = new Vector3 (x, y, playerPos.z);
+		// print ("(" + x + "," + y + "," + playerPos.z + ")");
+		// print ("(" + this.transform.position.x + "," + this.transform.position.y + "," + this.transform.position.z + ")");
+		//transform.position = player.transform.position;
+	}
+}
