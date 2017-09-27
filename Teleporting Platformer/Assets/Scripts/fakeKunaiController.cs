984f5523db2bda374889a3549283a7bb2ebe3a46
MichaelBingleBongle <mjnovet@gmail.com>
2017-09-27 04:02:55 -0400

    fake kunai prefab added

---
+++ Teleporting Platformer/Assets/Scripts/fakeKunaiController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fakeKunaiController : MonoBehaviour {
+	public GameObject _player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Destroy (gameObject);
+		}
+		if(Input.GetMouseButtonDown(1)){
+			_player.transform.position = transform.position;
+			Destroy (gameObject);
+		}
+	}
+}
