d6fd7c23172f5797939fb5927aa0c49e0c6af16a
David Huang <davidhuang972@gmail.com>
2017-09-27 10:40:37 +0200

    Add some more sprites

---
+++ Teleporting Platformer/Assets/Door.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+	public Sprite[] frames;
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D collision){
+		DoorActivate ();
+	}
+
+	void DoorActivate(){
+		int curScene = SceneManager.GetActiveScene ().buildIndex;
+		int curIndex = 0;
+
+		Debug.Log (curScene);
+
+		if (curIndex == 0) {
+			curIndex = 1;
+		} else if (curIndex == 1){
+			curIndex = 0;
+		}
+
+		spriteRenderer.sprite = frames [curIndex];
+	}
+}
--- Teleporting Platformer/Assets/Scripts/GrateElectricPrefab.cs
+++ Teleporting Platformer/Assets/Scripts/GrateElectricPrefab.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class GrateElectricPrefab : MonoBehaviour {
+	public Sprite[] frames;
 
+	public float framesPerSecond = 5;
+
+	SpriteRenderer spriteRenderer;
 	// Use this for initialization
 	void Start () {
-		
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		StartCoroutine (PlayAnimation ());
 	}
-	
+
+	IEnumerator PlayAnimation(){
+		int currentFrameIndex = 0;
+		while (currentFrameIndex < frames.Length) {
+			spriteRenderer.sprite = frames [currentFrameIndex];
+			yield return new WaitForSeconds(1f / framesPerSecond); // this halts the functions execution for x seconds. Can only be used in coroutines.
+			currentFrameIndex++;
+			if (currentFrameIndex == frames.Length) {
+				currentFrameIndex = currentFrameIndex % (int) framesPerSecond;
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void OnCollisionEnter2D(Collision2D _collision){
